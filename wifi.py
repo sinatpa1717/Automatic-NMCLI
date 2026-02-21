0b10a50dd9fcecb70be1260d301053c521d52c1d
Sina <you@example.com>
2026-02-21 19:06:37 +0330

    E

--- wifi.py
+++ wifi.py
@@ -1,5 +1,6 @@
 import os
 
+
 class MenuStart:
     def __init__(self):
         self.running = True
@@ -10,6 +11,12 @@ class MenuStart:
     def run_com(self, command: str):
         os.system(command)
 
+    def on_wifi(self):
+        self.run_com("nmcli radio wifi on")
+
+    def off_wifi(self):
+        self.run_com("nmcli radio wifi off")
+
     def connected_wifi(self):
         welcome_message = "Hello user\nWelcome to Automatic NMCLI Connected Tool"
         self.clear_screen()
@@ -18,12 +25,15 @@ class MenuStart:
         while self.running:
             menu_text = """
             ===============
-            
+
             [1] Show list wifi
             [2] Status wifi
             [3] Show password wifi connected
             [4] Connect SSID wifi
+            [on wifi]  Turn ON wifi
+            [off wifi] Turn OFF wifi
             [q] Exit program
+
             ===============
             """
             print(menu_text.upper())
@@ -40,11 +50,37 @@ class MenuStart:
                 self.clear_screen()
                 self.run_com("nmcli device wifi list")
 
-            elif choice in {"q", "exit"}:
+            elif choice in {"q", "exit", "end"}:
                 self.clear_screen()
                 self.running = False
                 print("PROGRAM OFF!")
                 break
+
+            elif choice == "2":
+                self.clear_screen()
+                self.run_com("nmcli device status")
+
+            elif choice == "3":
+                self.clear_screen()
+                self.run_com("nmcli device wifi show-password")
+
+            elif choice == "4":
+                try:
+                    name_ssid = input("SSID name: ").strip()
+                    password = input("Password: ").strip()
+                except KeyboardInterrupt:
+                    self.clear_screen()
+                    continue
+
+                self.run_com(
+                    f'nmcli device wifi connect "{name_ssid}" password "{password}"'
+                )
+
+            elif choice == "off wifi":
+                self.off_wifi()
+
+            elif choice == "on wifi":
+                self.on_wifi()
 
             else:
                 print("INVALID OPTION!")
